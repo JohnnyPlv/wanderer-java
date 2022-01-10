88331a99760ac355b7cca0c3bb17d27f347e094c
JohnnyPlv <h.plavka@gmail.com>
2022-01-10 19:15:33 +0100

    Added map class - still not sure if I will be using it in the future

---
+++ src/wanderer/Map.java
@@ -0,0 +1,18 @@
+package wanderer;
+
+import java.util.List;
+// TODO still unused, not sure if I will use it later, ATM the map is in AreaLeve class
+public class Map {
+    int[][] gameBoard = {
+            {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+            {0, 1, 0, 1, 0, 0, 1, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
+            {0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 1, 0, 0, 0}
+    };
+}
