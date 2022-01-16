01fc346cee382155d7137d5579a9bd5c500ae893
JohnnyPlv <h.plavka@gmail.com>
2022-01-16 18:43:40 +0100

    removed images from class, wont be doing pictures

--- src/wanderer/Dice.java
+++ src/wanderer/Dice.java
@@ -10,34 +10,10 @@ public class Dice implements Drawable {
     protected int posX;
     protected int posY;
     BufferedImage image;
-    protected String appearance;
     protected int valueForSp;
     protected int valueForDp;
 
     public Dice() { // TODO have to finish the pictures of Dice, this switch in constructor is not in use yet..
-        switch (5) {
-            case 1:
-                appearance = "img/boss.png";
-                break;
-            case 2:
-                appearance = "img/floor.png";
-                break;
-            case 3:
-                appearance = "img/hero-down.png";
-                break;
-            case 4:
-                appearance = "img/hero-left.png";
-                break;
-            case 5:
-                appearance = "img/hero-right.png";
-                break;
-            case 6:
-                appearance = "img/hero-up.png";
-                break;
-        }
-
-
-        this.positionedImage(appearance,posX,posY);
     }
 
     public int rollDiceOnSp () {
